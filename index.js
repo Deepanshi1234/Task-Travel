188b3cc7d885cc1e5db28ed3a0e6f23b94ca6dac
Deepanshi Kamboj <kambojdeepanshi671@gmail.com>
2023-10-12 17:27:03 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formbtn = document.querySelector('#login-btn');
+let loginform = document.querySelector('.login-form-container');
+let formclose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let formregister = document.querySelector('#register');
+let Registration = document.querySelector('.registeration');
+
+
+
+
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active')
+    loginform.classList.remove('active');
+    Registration.classList.remove('active');
+}
+searchBtn.addEventListener('click', () => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active')
+});
+
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+});
+
+formbtn.addEventListener('click', () => {
+    loginform.classList.add('active');
+});
+formclose.addEventListener('click', () => {
+    loginform.classList.remove('active');
+});
+formregister.addEventListener('click', () => {
+    Registration.classList.add('active');
+});
+formclose.addEventListener('click', () => {
+    Registration.classList.remove('active');
+});
